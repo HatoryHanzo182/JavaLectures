9ccdaf13ba04eeac42df6dd8181c08b74b144dd3
HatoryHanzo <scrinnpowerapp@gmail.com>
2023-09-20 21:32:12 +0300

    Implementation of the first task.

---
+++ src/main/java/step/learning/Dictionary.java
@@ -0,0 +1,2 @@
+package step.learning;public class Dictionary {
+}
---
+++ src/main/java/step/learning/ResultLecture.java
@@ -0,0 +1,9 @@
+package step.learning;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
