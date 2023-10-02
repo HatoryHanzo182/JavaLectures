72af697bc4de33541c42e99bb913f1c2d25a9763
HatoryHanzo <scrinnpowerapp@gmail.com>
2023-10-03 00:11:55 +0300

    Add RandomService implementation, use char[] array instead StringBuilder. Implement two named dependencies via @Provides.

--- src/main/java/step/learning/IOC/Services/Hash/Md5HashService.java
+++ src/main/java/step/learning/IOC/Services/Hash/Md5HashService.java
@@ -1,9 +1,12 @@
 package step.learning.IOC.Services;
 
+import com.google.inject.Singleton;
+
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+@Singleton
 public class Md5HashService implements IHashService
 {
     private final char[] _HEX_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
---
+++ src/main/java/step/learning/IOC/Services/Random/RandomServiceV1.java
@@ -0,0 +1,2 @@
+package step.learning.IOC.Services.Random;public class RandomServiceV1 {
+}
