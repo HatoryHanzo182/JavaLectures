bab665588635df0f11b6c8659163c09096d35bad
HatoryHanzo <scrinnpowerapp@gmail.com>
2023-10-04 21:24:16 +0300

    Implement the task of formation by means of multithreading pandigital number that consists of all digits 0-9. Order numbers do not play a role. Start 10 threads, each of which delays by 1s after which he adds his number to the common one "numbers" (String) and outputs an intermediate result:    added 7, result 1287 Provide all necessary synchronization measures. At the end, the summary is displayed once

--- src/main/java/step/learning/Async/Async.java
+++ src/main/java/step/learning/Async/Async.java
@@ -1,7 +1,97 @@
 package step.learning.Async;
 
+import java.awt.image.ImagingOpException;
+import java.util.Random;
+
 public class Async
 {
+    //
+    // + + + + + + + + + + Run3 + + + + + + + + + +
+    private String _constructed_number = "";
+    private Object _locker = new Object();
+
+    class Pandigital implements Runnable
+    {
+        private String _number;
+
+        public Pandigital(String number)
+        {
+            _number = number;
+        }
+
+        @Override
+        public void run()
+        {
+            try{ Thread.sleep(1000); }
+            catch(InterruptedException ex) { System.err.println(ex.getMessage()); }
+
+            synchronized (_locker)
+            {
+                _constructed_number += _number;
+                System.out.println("New num: (" + _number + ")  |  result: (" + _constructed_number + ")");
+            }
+        }
+    }
+
+    public void Run()
+    {
+        Thread[] threads = new Thread[10];
+
+        System.out.println("---------------------");
+
+        for(int i = 0; i < 10; i++)
+        {
+            threads[i] = new Thread(new Pandigital(Integer.toString(new Random().nextInt(10))));
+            threads[i].start();
+        }
+
+        try
+        {
+            for(int i = 0; i < 10; i++)
+                threads[i].join();
+        }
+        catch(InterruptedException ex) { System.err.println(ex.getMessage()); }
+
+        System.out.println("---------------------\nResult: { " + _constructed_number + " }");
+    }
+    // + + + + + + + + + + + + + + + + + + + + + + +
+
+
+
+
+
+
+
+    //
+    // + + + + + + + + + + Run1 + + + + + + + + + +
+    public void Run1()
+    {
+        System.out.println("Async demo!");
+
+        Thread thread1 = new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                System.out.println("Thread 1(start)..");
+
+                try { Thread.sleep(1000); }
+                catch (InterruptedException ex) { System.err.println(ex.getMessage()); }
+
+                System.err.println("Thread 1(finishes)..");
+            }
+        });
+        thread1.start();
+
+        try { thread1.join(); }
+        catch (InterruptedException ex) { System.err.println(ex.getMessage()); }
+
+        System.out.println("Async demo finishes");
+    }
+    // + + + + + + + + + + + + + + + + + + + + + + +
+
+    //
+    // + + + + + + + + + + Run2 + + + + + + + + + +
     private double _sum;
     private final Object _sum_locker = new Object();
     private final Object _atc_locker = new Object();
@@ -42,11 +132,11 @@ public class Async
 
                 if(is_last)
                     System.out.println("-------------------\nTotal: " + _sum);
-            } 
+            }
         }
     }
 
-    public void Run()
+    public void Run2()
     {
         int monthes = 12;
         Thread[] threads = new Thread[monthes];
@@ -69,29 +159,5 @@ public class Async
         //
         //System.out.println("-------------------\nTotal: " + _sum);
     }
-
-    public void Run1()
-    {
-        System.out.println("Async demo!");
-
-        Thread thread1 = new Thread(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                System.out.println("Thread 1(start)..");
-
-                try { Thread.sleep(1000); }
-                catch (InterruptedException ex) { System.err.println(ex.getMessage()); }
-
-                System.err.println("Thread 1(finishes)..");
-            }
-        });
-        thread1.start();
-
-        try { thread1.join(); }
-        catch (InterruptedException ex) { System.err.println(ex.getMessage()); }
-
-        System.out.println("Async demo finishes");
-    }
+    // + + + + + + + + + + + + + + + + + + + + + + +
 }
